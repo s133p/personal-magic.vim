47bad7be67c1bfaa27ae35ead8690dc5a568073d
s133p <darkchild50@gmail.com>
2016-12-09 12:11:34 -0800

    Updated templated for ds_cinder

--- templates/t.cpp
+++ templates/t.cpp
@@ -1,8 +1,53 @@
 #include "%POUND_INCL%"
 
+#include <ds/app/environment.h>
+#include <ds/ui/sprite/sprite_engine.h>
+#include <ds/debug/logger.h>
+
+#include "app/app_defs.h"
+#include "app/globals.h"
+#include "events/app_events.h"
+
+//#include "ds/ui/interface_xml/interface_xml_importer.h"
+
 namespace %NAMESPACE%{
 
-%CLASS_NAME%::%CLASS_NAME%(){
+**CLASSNAME**::**CLASSNAME**(Globals& g)
+	: ds::ui::Sprite(g.mEngine)
+	, mGlobals(g)
+	, mEventClient(g.mEngine.getNotifier(), [this](const ds::Event *m){ if(m) this->onAppEvent(*m); })
+{
+	animateOn();
+}
+
+void **CLASSNAME**::onAppEvent(const ds::Event& in_e){
+	if(in_e.mWhat == IdleEndedEvent::WHAT()){
+		const IdleEndedEvent& e((const IdleEndedEvent&)in_e);
+		animateOn();
+	} else if(in_e.mWhat == IdleStartedEvent::WHAT()){
+		animateOff();
+	}
+}
+
+void **CLASSNAME**::layout(){
+}
+
+void **CLASSNAME**::animateOn(){
+	show();
+	tweenOpacity(1.0f, mGlobals.getAnimDur());
+
+	// Recursively animate on any children, including the primary layout
+	//tweenAnimateOn(true, 0.0f, 0.05f);
+}
+
+void **CLASSNAME**::animateOff(){
+	tweenOpacity(0.0f, mGlobals.getAnimDur(), 0.0f, ci::EaseNone(), [this]{hide(); });
+}
+
+void **CLASSNAME**::updateServer(const ds::UpdateParams& p){
+	ds::ui::Sprite::updateServer(p);
+
+	// any changes for this frame happen here
 }
 
 }//end namespace %NAMESPACE%
--- templates/t.h
+++ templates/t.h
@@ -1,17 +1,36 @@
+#pragma once
 #ifndef %INCL_GUARD%
 #define %INCL_GUARD%
 
+#include <ds/ui/sprite/sprite.h>
+#include <ds/app/event_client.h>
+
 namespace %NAMESPACE%{
 
-/* %CLASS_NAME%
- *  ...
- */
-class %CLASS_NAME%{
+class Globals;
+
+/**
+* \class %NAMESPACE%::%CLASSNAME%
+*
+*/
+class **CLASSNAME** final : public ds::ui::Sprite  {
 public:
-    %CLASS_NAME%();
+	**CLASSNAME**(Globals& g);
 
 private:
-    // ...
+	void								onAppEvent(const ds::Event&);
+
+	virtual void						updateServer(const ds::UpdateParams& p);
+
+	void								animateOn();
+	void								animateOff();
+
+	void								layout();
+
+	Globals&							mGlobals;
+
+	ds::EventClient						mEventClient;
+
 };
 
 }//end namespace %NAMESPACE%
